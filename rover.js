5695fab1a52938ad0bca0d74b27bbfb1e78c45d1
unknown <danc46436@gmail.com>
2024-08-22 22:37:53 -0500

    Updated Assignment 3

--- rover.js
+++ rover.js
@@ -1,5 +1,47 @@
 class Rover {
    // Write code here!
+   constructor(position)   {
+      this.position = position;
+      this.mode = "NORMAL";
+      this.generatorWatts =110;
+   }
+
+   receiveMessage(message) {
+      let results = message.commands.map(command => {
+         if(command.commandType === 'STATUS_CHECK') {
+
+         return {
+             completed: true, 
+             roverStatus: {
+               mode: this.mode,
+               generatorWatts: this.generatorWatts,
+               position: this.position
+             }
+            };
+         } else if (command.commandType === 'MODE_CHANGE') {
+            this.mode =command.value;
+            return { completed: true };
+         } else if (command.commandType === 'MOVE') {
+            if (this.mode === 'LOW_POWER')
+         
+      {
+         return { completed: false };
+      } else {
+         this.position = command.value;
+         return { completed : true };
+      }
+   } else {
+      return { completed: true };
+   }
+
+   });
+
+      return {
+         message: message.name,
+         results: results
+
+      };
+   }
 }
 
 module.exports = Rover;
